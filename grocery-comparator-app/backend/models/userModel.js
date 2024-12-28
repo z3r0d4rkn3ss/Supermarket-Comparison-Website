96f8beb7571ce19fba1ac575a635635b67985225
z3r0 d4rkn3ss <31696563+z3r0d4rkn3ss@users.noreply.github.com>
2024-12-28 23:51:48 +0000

    Update userModel.js

--- grocery-comparator-app/backend/models/userModel.js
+++ grocery-comparator-app/backend/models/userModel.js
@@ -1,10 +1,19 @@
-// User schema for MongoDB or SQL
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = mongoose.model('User', userSchema);

86c5febd25ec65a9f3b98d734687f0f7e999b764
z3r0 d4rkn3ss <31696563+z3r0d4rkn3ss@users.noreply.github.com>
2024-12-28 21:40:25 +0000

    Update index.js

--- grocery-comparator-app/frontend/src/index.js
+++ grocery-comparator-app/frontend/src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css'; // Import the styles
+import App from './App'; // Import the main App component
 
+const root = ReactDOM.createRoot(document.getElementById('root')); // Get the root div from index.html
+root.render(
+  <React.StrictMode>
+    <App />  {/* Render the App component */}
+  </React.StrictMode>
+);

5f184d7c7420c9dc0ba6c65c0883f734d755f17f
z3r0 d4rkn3ss <31696563+z3r0d4rkn3ss@users.noreply.github.com>
2024-12-28 23:52:47 +0000

    Update basketModel.js

--- grocery-comparator-app/backend/models/basketModel.js
+++ grocery-comparator-app/backend/models/basketModel.js
@@ -1,10 +1,28 @@
-// Basket schema to store user's basket
-
 const mongoose = require('mongoose');
 
 const basketSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    items: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = mongoose.model('Basket', basketSchema);

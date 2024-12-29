7fca7e98e2493bc6b34a28aaeee2177c98ce149c
z3r0 d4rkn3ss <31696563+z3r0d4rkn3ss@users.noreply.github.com>
2024-12-29 01:19:53 +0000

    Create background.js

---
+++ grocery-comparator-firefox-extension/background.js
@@ -0,0 +1,6 @@
+// background.js
+
+// You can store and manage basket data here if needed.
+chrome.runtime.onInstalled.addListener(function() {
+  console.log('Grocery Comparator extension installed!');
+});
